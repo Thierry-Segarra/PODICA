95a4b360a1ab7dfde4f3d64af5217440e6ed1bd0
Tetdoss13 <97156314+Tetdoss13@users.noreply.github.com>
2022-09-28 10:22:10 +0200

    Update parametre-compte.php
    update bdd commentaire
    modif texte parametre compte

--- parametre-compte.php
+++ parametre-compte.php
@@ -17,19 +17,19 @@
         ?>
         <div id="container">            
             <form action="parametre-compte-form.php" method="POST">
-                <h1>modification compte</h1>
+                <h1>Paramètres du compte</h1>
                 
                 <label><b>Changer le Nom d'utilisateur</b></label>
                 <input type="text" placeholder="Entrer le nouveau nom d'utilisateur" name="username" maxlength="13" >
                 
                 <label><b>Changer l'Email</b></label>
-                <input type="email" placeholder="Entrer votre email" name="email">
+                <input type="email" placeholder="Entrer le nouvelle email" name="email">
 
                 <label><b>Mot de passe actuel</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+                <input type="password" placeholder="Entrer le mot de passe actuel" name="password" required>
 
 
-                <input type="submit" id='submit' value="INSCRIPTION" >
+                <input type="submit" id='submit' value="Mettre à jour le compte" >
                 <?php
                 if(isset($_GET['erreur'])){
                     $err = $_GET['erreur'];
