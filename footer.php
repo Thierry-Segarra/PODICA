c566f93abbc635da40791965d1547481ba8f7776
Romain François <100940085+HoySenpai@users.noreply.github.com>
2022-09-30 11:03:54 +0200

    footer final

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 
 <head>
-  <link rel="stylesheet" href="css/style-footer.css">
+  <link rel="stylesheet" href="style-footer.css">
 </head>
 
 <body>
     <div class="footer">
             <div class="contain">
                 <div class="row">
-                    <div class="col">
+                    <div class="col-left">
                         <h1>Nous contacter</h1>
                         <ul>
                             <li>Support</li>
                             <li>00.00.00.00.00</li>
                         </ul>
                     </div>
-                    <div class="col-img">
-                    <img class="img-footer" src="image/Podica_logo.png">
-²                   </div>                   
-                    <div class="col-social">
+                    <div class="col-center">
+                    <img class="img-footer" src="../image/Podica_logo.png">
+                   </div>                   
+                    <div class="col-right">
                          <h1>Nos réseaux sociaux</h1>
                         <ul>
                             <li><a href="#">Facebook</a></li>
@@ -27,7 +27,28 @@
                         </ul>
                     </div>
                 </div>
+                <hr>
+                <div class="row">
+                    <div class="col-left">
+                    <h1>Confidentialité</h1>
+                        <ul>
+                            <li>Politique de confidentialité</li>
+                        </ul>
+                    </div>
+                    <div class="col-center">
+                        <h1>Mentions légales</h1>
+                        <ul>
+                            <li>Conditions générales</li>
+                        </ul>
+                    </div>
+                    <div class="col-right">
+                    <h1>Cookies</h1>
+                        <ul>
+                            <li>Gestion des cookies</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-    </div>
+        </div>
 </body>
 </html>
