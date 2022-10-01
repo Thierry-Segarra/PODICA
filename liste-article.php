568c56d1d23aa833e22956f2f137e1e0b38e0728
Romain François <100940085+HoySenpai@users.noreply.github.com>
2022-10-01 12:12:45 +0200

    css article finit

--- liste-article.php
+++ liste-article.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
+<body>
+<header>
+<?php include('header.php')?>
+<link rel="stylesheet" href="css/style-articles.css">
+</header>
+
+    <div class="row-bienvenue">
+        <div class="bienvenue">
+            <h1>Bienvenue sur Podica</h1>
+        </div>
+    </div>
+    <br>
+
+    <div class="wrapper-article">
 
 <?php
 include('connect.php');
@@ -11,21 +25,34 @@ $count = $reponse['count(*)'];
 
 if($count!=0){
 
-        $requete="Select id_article, titre, C.nom, description, contenue from article A INNER JOIN categorie C ON A.id_categorie = C.id_categorie";
+        $requete="Select id_article, titre, C.nom, description, U.pseudo, date_publication from article A INNER JOIN categorie C ON A.id_categorie = C.id_categorie INNER JOIN user U ON A.id_user = U.id_user";
         $exec_requete2 = mysqli_query($db,$requete);
+       
+ 
+        while($row = mysqli_fetch_assoc($exec_requete2)){
+        echo'
+            <div class="article">
+            <div class="row-td">
+       <a class="titre" href="afficher-article.php?id=' . $row["id_article"] . '">' . $row["titre"]. '</a>
+       <p>' . $row["date_publication"] . '</p></div>
+       
+       <div class="row-desc">
+       <p>' . $row["description"] . '</p>
+       </div>
 
-    while($row = mysqli_fetch_assoc($exec_requete2)){
-        
-        echo'<div style="border: solid black 2px">
-       <a href="afficher-article.php?id=' . $row["id_article"] . '">Titre : ' . $row["titre"] . '</a>
-       <p>Catégorie : ' . $row["nom"] . '</p>
-       <p>Descrisption : ' . $row["description"] . '</p>
-       <p>Contenu: ' . $row["contenue"] . '</p>
-       </div><br><br>';
+       <div class="row-ncat">
+            <p>' . $row["nom"] . '</p>
+            <p>' . $row["pseudo"] . '</p>
+       </div>
+       </div>';
         
     };
 
 };
 ?>
-
-</html>
+</div>
+<footer>
+    <?php include('footer.php')?>
+</footer>
+</body>
+</html> 
