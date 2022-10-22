1b9ee09dd7d337de0f3fec2c643eecddcca2c266
Romain François <100940085+HoySenpai@users.noreply.github.com>
2022-10-22 15:01:47 +0200

    changement chart

--- test css afficher article/commentaire.php
+++ test css afficher article/commentaire.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style-commentaire.css">
-    <title>Podica</title>
-</head>
-
-<body>
-
-    <section>
-
-        <form method="POST" action='envoie_com.php'>
+       <form method="POST" action='envoie_com.php' class="form-com">
             <div class="commentaire">
              <div class="titre">
-                <label>Commentaire</label><br>
+                <label> Poster un commentaire</label><br>
                </div>
                     <div class="textarea">
                     <textarea name="valide_com"cols="30" rows="1" style="resize:none; width:100%; height:200px; border:0"></textarea>
@@ -27,10 +12,3 @@
                         <button type="submit" name="submit">Publier le commentaire</button>
                         </div>
         </form>
-
-    </section>
-
-</body>
-
-
-</html>
